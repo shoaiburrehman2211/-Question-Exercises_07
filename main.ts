0ea729bca15d1112b7e9b3a9f760c2a6e77266aa
Shoaib-ur-Rehman <160622745+shoaiburrehman2211@users.noreply.github.com>
2024-04-22 02:26:33 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,10 @@
+/*Write addition, subtraction, multiplication, and division operations that each result in the 
+number 8. Be sure to enclose your operations in print statements to see the results.
+*/
+
+const addition: number = 4 + 4;
+const subtraction: number = 10 - 2;
+const multiplication: number = 2 * 4;
+const division: number = 32 / 4;
+
+console.log(addition, subtraction, multiplication, division);
